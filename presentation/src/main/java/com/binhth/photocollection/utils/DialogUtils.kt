639c6b4acee95acf232c4e2c88fe04a8cebdda00
mrprona92 <mrprona92@gmail.com>
2018-10-29 15:38:45 +0700

    [Task6-PhotoEdit] Fix comment

--- presentation/src/main/java/com/binhth/photocollection/utils/DialogUtils.kt
+++ presentation/src/main/java/com/binhth/photocollection/utils/DialogUtils.kt
@@ -12,14 +12,14 @@ object DialogUtils {
 
     fun showSaveDialog(context: Context, saveEditListener: SaveEditListener) {
         val builder = AlertDialog.Builder(context)
-        builder.setMessage(context.getString(R.string.dialog_quest_save_image))
-        builder.setPositiveButton(context.getString(R.string.dialog_save)) { _, _ -> saveEditListener.saveImage() }
-        builder.setNegativeButton(
-            context.getString(R.string.dialog_cancel)
-        ) { dialog, _ -> dialog.dismiss() }
-
-        builder.setNeutralButton(context.getString(R.string.dialog_discard)) { _, _ -> saveEditListener.finish() }
-        builder.create().show()
+        builder.apply {
+            setMessage(context.getString(R.string.dialog_quest_save_image))
+            setPositiveButton(context.getString(R.string.dialog_save)) { _, _ -> saveEditListener.saveImage() }
+            setNegativeButton(
+                context.getString(R.string.dialog_cancel)
+            ) { dialog, _ -> dialog.dismiss() }
+            setNeutralButton(context.getString(R.string.dialog_discard)) { _, _ -> saveEditListener.finish() }
+        }.create().show()
     }
 
     interface SaveEditListener {
